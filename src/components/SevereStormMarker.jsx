6e997b5baff7f1b15c51a647672f7d2c7599d3c5
Joshua Rully <joshrully@gmail.com>
2021-09-09 15:42:10 -0400

    finish up severeStorms marker

--- src/components/SevereStormMarker.jsx
+++ src/components/SevereStormMarker.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Rectangle, Popup } from 'react-leaflet';
 
 function SevereStormMarker({ onClick, position, id, title }) {
+  const blueOptions = {color: 'blue'};
   return (
     <Rectangle 
       bounds={position}
       onClick={onClick}
+      pathOptions={blueOptions}
     >
       <Popup>
         <h1>Title: {title}</h1>
